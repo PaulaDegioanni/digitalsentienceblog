4490f0db1c0fefd33b11334567e1b56ca166cd98
paudegio <pauladegioanni@gmail.com>
2025-06-05 19:30:34 +0200

    Pull articles from notion database

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,16 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+import { notionLoader } from "notion-astro-loader";
+import {
+  notionPageSchema,
+  propertySchema,
+  transformedPropertySchema,
+} from "notion-astro-loader/schemas";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+  loader: notionLoader({
+    auth: import.meta.env.NOTION_TOKEN,
+    database_id: import.meta.env.NOTION_DATABASE_ID
+  }),
 });
 
 export const collections = { blog };
